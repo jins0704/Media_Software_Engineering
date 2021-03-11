badb68ac856b235f35436cda2c4210c6bca1e22e
hongjinseok <sldkdl111@ajou.ac.kr>
2021-03-11 18:06:23 +0900

    add task5,task6

---
+++ Lab1/Lab1_Task6/src/StudentTest.java
@@ -0,0 +1,9 @@
+public class StudentTest {
+    public static void main(String[] args){
+        Student st = new Student();
+        st.setAge(25);
+        st.setStudentID(123123);
+        st.setGrade(3.2);
+        st.setName("jjj");
+    }
+}
